377023bd4cb9860798a8ea7d39379fd73c8a5fb2
Osama Alghanmi <oalghnmi@gmail.com>
2022-05-28 07:02:16 +0300

    models

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if (environment.production) {
+  enableProdMode();
+}
+
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
@@ -12,9 +16,5 @@ const providers = [
   { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
 ];
 
-if (environment.production) {
-  enableProdMode();
-}
-
 platformBrowserDynamic(providers).bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
